60def1c82f0fa1ad44986554a1fbe758f9ff9d1e
mateus.cmartins <mateus.cmartins@hotmail.com>
2023-03-30 22:08:54 -0300

    Seção de riscos ocupacionais

--- frontend/src/pages/NovaPT/index.js
+++ frontend/src/pages/NovaPT/index.js
@@ -8,7 +8,7 @@ export default function NovaPT() {
                 <header>
                 <div>LOGO</div>
                 <div className='links'>
-
+                    
                     <a href='/'>
                         Logout
                     </a>
@@ -28,7 +28,12 @@ export default function NovaPT() {
                         <section className='conteudo-esquerda'>
                             <label className='legenda'>Número da Ordem de Serviço</label>
                             <input type="text" placeholder='Nº da Ordem de Serviço' required></input>
-
+                            <label className='legenda'>Descrição do serviço</label>
+                            <textarea rows="5"/>
+                            
+                        </section>
+                        <section className='conteudo-direita'>
+                            
                             <div className='secao-dividida'>
                                 <div id='data-inicio'>
                                     <label className='legenda'>Data de início</label>
@@ -66,12 +71,34 @@ export default function NovaPT() {
                                 </div>
                             </div>
                         </section>
-                        <section className='conteudo-direita'>
-                            <label className='legenda'>Descrição do serviço</label>
-                            <input type="textarea"></input>
-                        </section>
                 </fieldset>
-    
+
+
+                <h2>Riscos envolvidos</h2>
+                <fieldset className='content' id='riscos'>
+                    <section className='conteudo-esquerda'>
+                        <input type="checkbox" value="movimentacao-guincho-plataforma"/>
+                        <label for="movimentacao-guincho-plataforma" > Movimentação com uso de guincho e plataforma </label><br></br>
+                        <input type="checkbox" value="manutencao-civil"/>
+                        <label for="manutencao-civil" > Manutenção civil </label><br></br>
+                        <input type="checkbox" value="gases-explosivos"/>
+                        <label for="gases-explosivos" > Gases, explosivos e/ou líquidos inflamáveis </label><br></br>
+                        <input type="checkbox" value="altura"/>
+                        <label for="altura" > Altura, telhados e/ou níveis elevados </label><br></br>
+                        <input type="checkbox" value="demolicao-escavacao"/>
+                        <label for="demolicao-escavacao" > Demolição e/ou Escavação </label><br></br>
+                    </section> 
+                    <section className='conteudo-direita'>
+                        <input type="checkbox" value="eletricidade"/>
+                        <label for="eletricidade" > Eletricidade </label><br></br>
+                        <input type="checkbox" value="trabalho-a-quente"/>
+                        <label for="trabalho-a-quente" > Trabalho à quente </label><br></br>
+                        <input type="checkbox" value="local-confinado"/>
+                        <label for="local-confinado" > Local confinado </label><br></br>
+                        <label>Outro: </label><br></br>
+                        <input type="text"></input>                       
+                    </section>       
+                </fieldset>
             </form>
             
 
